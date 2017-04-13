c36ef7441be3d2681944995996446a2d987f8e52
alsoraimi <alsoraimi46@gmail.com>
2017-04-13 16:09:58 -0400

    did a, b, c and D from number 1

--- nested loop/Program.cs
+++ nested loop/Program.cs
@@ -10,11 +10,39 @@ namespace nested_loop
     {
         static void Main(string[] args)
         {
-            StairCase(8);
+            //StairCase(8);
+            //Staircase2(8);
+            // Staircase3(8);
+            Staircase4(8);
+        }
+
+        public static void Staircase(int n) //1 b
+        {
+            {
+                for (int i = n; i >= 1; i--)
+                {
+                    Console.WriteLine(new String('#', i) + new String(' ', n + i));
+
+
+                }
+
+            }
 
         }
 
-        public static void StairCase(int n)
+        public static void Staircase2(int n) // 1A
+
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(new String('#', i) + new String(' ', n - i));
+
+
+            }
+
+        }
+
+        public static void StairCase3(int n)// problem 1D
         {
             for (int i = 1; i <= n; i++)
             {
@@ -23,5 +51,19 @@ namespace nested_loop
 
             }
         }
+
+        public static void Staircase4(int n)// 1C
+
+        {
+            for (int i = n; i >= 1; i--)
+            {
+                Console.WriteLine(new String(' ', n - i) + new String('#', i));
+
+
+            }
+
+        }
+
     }
 }
+
