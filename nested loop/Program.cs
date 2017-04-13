1694634194b0c61d00a3e666f21a0515ab63c14d
alsoraimi <alsoraimi46@gmail.com>
2017-04-13 15:22:45 -0400

    initial commit

---
+++ nested loop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nested_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StairCase(8);
+
+        }
+
+        public static void StairCase(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(new String(' ', n - i) + new String('#', i));
+
+
+            }
+        }
+    }
+}
